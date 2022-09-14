e00d56d561937dcaabdc7faac745edf8bff95866
Tobey Blaber <contact@tobey.me>
2022-09-14 20:07:11 +0100

    Initial commit

---
+++ src/cli.ts
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+
+export const cli = yargs(hideBin(process.argv))
+    .parserConfiguration({ 'duplicate-arguments-array': false })
+    .commandDir('commands', { extensions: ['ts', 'js'] })
+    .strictCommands()
+    .demandCommand(1, '')
+    .help()
+    .version()
+    .alias('h', 'help')
+    .group(['help', 'version'], 'Help:');
+
+(async () => {
+    try {
+        await cli.argv;
+    } catch (e) {
+        process.exitCode = 1;
+        console.error(e);
+    }
+})();
