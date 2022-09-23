02414a6b703ae7339c84fa6ca052fabf95965cbf
Tobey Blaber <contact@tobey.me>
2022-09-23 11:39:30 +0100

    symlink tests

--- test/utils.isBrokenSymlink.spec.ts
+++ test/utils.isBrokenSymlink.spec.ts
@@ -1,8 +1,11 @@
-import { resolve } from 'node:path';
+import { ensureSymlink, ensureDir, rm, unlink } from 'fs-extra';
+import { join, resolve } from 'node:path';
 
 import { isBrokenSymlink } from '../src/utils';
 
-// TODO: write tests against symbolic links!
+const testDir = resolve(join('test', '.utils.isBrokenSymlink'));
+beforeAll(async () => await ensureDir(testDir));
+afterAll(async () => await rm(testDir, { recursive: true, force: true }));
 
 describe('when path points to a file', () => {
     test('should return false', async () => {
@@ -13,5 +16,43 @@ describe('when path points to a file', () => {
 describe('when path points to a non-existant path', () => {
     test('should return false', async () => {
         expect(await isBrokenSymlink(resolve('foo.bar'))).toBe(false);
+    });
+});
+
+describe('when path points to a valid symbolic link', () => {
+    const src = resolve(join(testDir, 'foo'));
+    const target = resolve(join(testDir, 'bar'));
+
+    beforeAll(async () => {
+        await ensureDir(src);
+        await ensureSymlink(src, target, 'junction');
+    });
+
+    afterAll(async () => {
+        await unlink(target);
+        await rm(src, { recursive: true, force: true });
+    });
+
+    test('should return false', async () => {
+        expect(await isBrokenSymlink(target)).toBe(false);
+    });
+});
+
+describe('when path points to a broken symbolic link', () => {
+    const src = resolve(join(testDir, 'foobar'));
+    const target = resolve(join(testDir, 'barfoo'));
+
+    beforeAll(async () => {
+        await ensureDir(src);
+        await ensureSymlink(src, target, 'junction');
+        await rm(src, { recursive: true, force: true });
+    });
+
+    afterAll(async () => {
+        await unlink(target);
+    });
+
+    test('should return false', async () => {
+        expect(await isBrokenSymlink(target)).toBe(true);
     });
 });
---
+++ test/utils.unlinkBrokenSymlinks.spec.ts
@@ -0,0 +1,82 @@
+import {
+    ensureSymlink,
+    ensureDir,
+    rm,
+    unlink,
+    stat,
+    lstat,
+    readlink,
+} from 'fs-extra';
+import { join, resolve } from 'node:path';
+
+import { unlinkBrokenSymlinks } from '../src/utils';
+
+const testDir = resolve(join('test', '.utils.unlinkBrokenSymlinks'));
+beforeAll(async () => await ensureDir(testDir));
+afterAll(async () => await rm(testDir, { recursive: true, force: true }));
+
+describe('when dir points to a file', () => {
+    test('should throw error', async () => {
+        try {
+            await unlinkBrokenSymlinks(resolve('tsconfig.json'));
+            fail('must throw');
+        } catch (e) {
+            expect(e).toHaveProperty('code', 'ENOTDIR');
+        }
+    });
+});
+
+describe('when dir points to an empty directory', () => {
+    const dir = resolve(join(testDir, 'foo'));
+    beforeAll(async () => await ensureDir(dir));
+    afterAll(async () => await rm(dir, { recursive: true, force: true }));
+
+    test('should complete normally', () => {
+        expect(unlinkBrokenSymlinks(testDir)).resolves.not.toThrow();
+    });
+});
+
+describe('when dir points to a directory containing valid symlinks', () => {
+    const dir = resolve(join(testDir, 'bar'));
+    const src = resolve(join(dir, 'src'));
+    const target = resolve(join(dir, 'target'));
+
+    beforeAll(async () => {
+        await ensureDir(src);
+        await ensureSymlink(src, target, 'junction');
+    });
+    afterAll(async () => {
+        await unlink(target);
+        await rm(dir, { recursive: true, force: true });
+    });
+
+    test('should complete normally without deleting any symlinks', async () => {
+        await unlinkBrokenSymlinks(dir);
+        expect((await stat(dir)).isDirectory()).toBe(true);
+        expect((await stat(src)).isDirectory()).toBe(true);
+        expect((await lstat(target)).isSymbolicLink()).toBe(true);
+        expect(resolve(await readlink(target))).toBe(src);
+    });
+});
+
+describe('when dir points to a directory containing invalid symlinks', () => {
+    const dir = resolve(join(testDir, 'foobar'));
+    const src = resolve(join(dir, 'src'));
+    const target = resolve(join(dir, 'target'));
+
+    beforeAll(async () => {
+        await ensureDir(src);
+        await ensureSymlink(src, target, 'junction');
+        await rm(src, { recursive: true, force: true });
+    });
+    afterAll(async () => {
+        await rm(dir, { recursive: true, force: true });
+    });
+
+    test('should complete and to have removed the broken symlink', async () => {
+        await unlinkBrokenSymlinks(dir);
+        expect((await stat(dir)).isDirectory()).toBe(true);
+        expect(stat(src)).rejects.toThrow();
+        expect(lstat(target)).rejects.toThrow();
+    });
+});
